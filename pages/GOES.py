31416f5d9407fc41c7bca8f6f3cec79e5c44ffa6
Shubham Goyal <goyal.shubh15@gmail.com>
2023-03-02 17:06:35 -0500

    api_limit and backend issues fix

--- pages/GOES.py
+++ pages/GOES.py
@@ -38,56 +38,91 @@ if 'database.db' not in os.listdir(os.getcwd()):
 
 FASTAPI_url='http://localhost:8000/goes_station'
 response=requests.get(FASTAPI_url,headers=headers)
-
+station_box=None
 if response.status_code == 200:
     station=response.json()
     station_list=station['Stations']
     station_box = st.selectbox(
         'Station:',station_list)
+    
+else:
+    st.error('Either you have not logged in or else your session has expired.', icon="🚨")
+    
+
+if station_box!=None:
     FASTAPI_url='http://localhost:8000/goes_years'
     data={'station':station_box}
     response=requests.get(FASTAPI_url,json=data,headers=headers)
+    year_box=None        
     if response.status_code==200:
         year=response.json()
         year_list=year['Year']
         year_box = st.selectbox('Year:',year_list)
+    else:
+        st.error('Either you have not logged in or else your session has expired.', icon="🚨")
+        
+            
+    if year_box!=None:
         FASTAPI_url='http://localhost:8000/goes_days'
         data={'station':station_box,'year':year_box}
         response=requests.get(FASTAPI_url,json=data,headers=headers)
+        day_box=None
         if response.status_code==200:
             day=response.json()
             day_list=day['Day']
             day_box = st.selectbox('Day:',day_list)
+        else:
+            st.error('Either you have not logged in or else your session has expired.', icon="🚨")
+            
+        
+        if day_box!=None:
             FASTAPI_url='http://localhost:8000/goes_hours'
             data={'station':station_box,'year':year_box, 'day':day_box}
             response=requests.get(FASTAPI_url,json=data,headers=headers)
+            hour_box=None
             if response.status_code==200:
                 hour=response.json()
                 hour_list=hour['Hour']
                 hour_box = st.selectbox('Hour:',hour_list)
+            else:
+                st.error('Either you have not logged in or else your session has expired.', icon="🚨")
+                
+            
+            if hour_box!=None:
                 FASTAPI_url='http://localhost:8000/goes_files'
                 data={'station':station_box,'year':year_box, 'day':day_box,'hour':hour_box}
                 response=requests.get(FASTAPI_url,json=data,headers=headers)
+                file_box=None
                 if response.status_code==200:
                     file=response.json()
                     file_list=file['Files']
                     file_box = st.selectbox('Files:',file_list)
                 else:
                     st.error('Either you have not logged in or else your session has expired.', icon="🚨")
+                    
 
                 if st.button('Submit'):
+                    
                     FASTAPI_url='http://localhost:8000/user_api_status'
                     input={'api_name':'goes_feature'}
                     response=requests.post(FASTAPI_url,json=input,headers=headers)
                     if response.status_code==200:
-                        FASTAPI_url='http://localhost:8000/goes_fetch_url'
-                        input={'station':station_box,'year':year_box,'day':day_box,'hour':hour_box,'file':file_box}
-                        response=requests.post(FASTAPI_url,json=input,headers=headers)
-                        if response.status_code==200:
-                            goes_url=response.json()
-                            goes_url=goes_url['NOAAURL']
-                            st.markdown("**Generated URL**")
-                            st.write(goes_url)
+                        with st.spinner('Generating Public S3 Link...'):
+                            FASTAPI_url='http://localhost:8000/goes_fetch_url'
+                            input={'station':station_box,'year':year_box,'day':day_box,'hour':hour_box,'file':file_box}
+                            response=requests.post(FASTAPI_url,json=input,headers=headers)
+                            if response.status_code==200:
+                                goes_url=response.json()
+                                goes_url=goes_url['NOAAURL']
+                                st.markdown("**Generated URL**")
+                                st.write(goes_url)
+                            elif response.status_code == 401:
+                                st.error('Either you have not logged in or else your session has expired.', icon="🚨")
+                            else:
+                                st.markdown("**Error generating NOAA GOES URL**")
+                                 
+                        
+                        with st.spinner('Generating Custom Link...'):        
                             FASTAPI_url='http://localhost:8000/goes_AWS_url'
                             input={'station':station_box,'year':year_box,'day':day_box,'hour':hour_box,'file':file_box}
                             response=requests.post(FASTAPI_url,json=input,headers=headers)
@@ -100,17 +135,8 @@ if response.status_code == 200:
                                 st.error('Either you have not logged in or else your session has expired.', icon="🚨")
                             else:
                                 st.markdown("**Error generating AWS S3 URL**")
-                        elif response.status_code == 401:
-                            st.error('Either you have not logged in or else your session has expired.', icon="🚨")
-                        else:
-                            st.markdown("**Error generating NOAA GOES URL**")
                     else:
-                        st.error('Either you have not logged in or else your session has expired.', icon="🚨")
-            else:
-                st.error('Either you have not logged in or else your session has expired.', icon="🚨")
-        else:
-            st.error('Either you have not logged in or else your session has expired.', icon="🚨")
-    else:
-        st.error('Either you have not logged in or else your session has expired.', icon="🚨")
-else:
-    st.error('Either you have not logged in or else your session has expired.', icon="🚨")
+                        st.error('Looks like your limit has exceeded!', icon="🚨")
+                       
+                        
+                            
