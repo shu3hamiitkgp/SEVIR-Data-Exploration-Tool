8f2e018486d0ad9873920096c912ba73cd1c1882
Shubham Goyal <goyal.shubh15@gmail.com>
2023-03-01 20:26:29 -0500

    user activity api done

--- pages/Nexrad_plot.py
+++ pages/Nexrad_plot.py
@@ -19,14 +19,21 @@ ACCESS_TOKEN = os.environ["access_token"]
 headers = {"Authorization": f"Bearer {ACCESS_TOKEN}"}
 
 st.title("NEXRAD Station Locations")
-response = requests.post('http://localhost:8000/retrieve_plot_data',headers=headers)
+FASTAPI_URL='http://localhost:8000/user_api_status'
+input={'api_name':'goes_filename'}
+response=requests.post(FASTAPI_URL,json=input,headers=headers)
+            
+if response.status_code==200:
+    response = requests.post('http://localhost:8000/retrieve_plot_data',headers=headers)
 
-if response.status_code == 200:
-    df = pd.DataFrame(response.json()['df_dict'])
-    #sql_query = pd.read_sql('SELECT * FROM NEXRAD_PLOT', con)
-    #df = pd.DataFrame(sql_query, columns = ['Id', 'State', 'City','ICAO_Location_Identifier','Coordinates','Lat','Lon'])
-    fig = px.scatter_geo(df,lat='Lat',lon='Lon')
-    fig.update_layout(title = 'Nexrad Locations', title_x=0.5)
-    st.plotly_chart(fig, use_container_width=True)
+    if response.status_code == 200:
+        df = pd.DataFrame(response.json()['df_dict'])
+        #sql_query = pd.read_sql('SELECT * FROM NEXRAD_PLOT', con)
+        #df = pd.DataFrame(sql_query, columns = ['Id', 'State', 'City','ICAO_Location_Identifier','Coordinates','Lat','Lon'])
+        fig = px.scatter_geo(df,lat='Lat',lon='Lon')
+        fig.update_layout(title = 'Nexrad Locations', title_x=0.5)
+        st.plotly_chart(fig, use_container_width=True)
+    else:
+        st.error('Either you have not logged in or else your session has expired.', icon="🚨")
 else:
-    st.error('Either you have not logged in or else your session has expired.', icon="🚨")
+    st.error('Either you have not logged in or else you have exceeded the api limit.', icon="🚨")
